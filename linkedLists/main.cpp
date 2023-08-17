0fb02be3a1c6be48073f89bc6c6edcfdf048e87a
yahya chami <123872563+dzap00@users.noreply.github.com>
2023-08-17 20:06:16 +0100

    Create main.cpp

---
+++ linkedLists/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "linked_list.h"
+
+int main()
+{
+	auto* list = new linkedList;
+	auto* Arr = new linkedList;
+	auto* ptr = new Node;
+	for (int i = 0; i < 6; i++)
+		list->insertByTail(i + 1);
+    //list->Display();
+} 
