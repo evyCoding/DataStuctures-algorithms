408d98754d84b7c6ba3d8a9a44ff7a928036782a
yahya chami <123872563+dzap00@users.noreply.github.com>
2023-08-18 20:52:17 +0100

    Update linkedLists.cpp

--- linkedLists/linkedLists.cpp
+++ linkedLists/linkedLists.cpp
@@ -1,76 +1,11 @@
 #include "linked_list.h"
 
-
-//todo -> circularLinkedLists :
-
-CircularLinkedList :: CircularLinkedList(void) {
-	Head->next = Head;
-	Head->Data = 0;
-}
-
-void CircularLinkedList::create(std::vector<int> vex) {
-	for (int i = 0; i < vex.size(); i++)
-		insert(vex[i]);
-}
-
-void CircularLinkedList :: Display(void) {
-	auto* keepTrack = Head->next;
-	while (keepTrack != Head) {
-		std::cout << keepTrack->Data << std::endl;
-		keepTrack = keepTrack->next;
-	}
-}
-
-void CircularLinkedList::insert(int value) {
-	auto* n = new Node;
-	auto* keepTrack = Head;
-	n->Data = value;
-	n->next = Head;
-	while (keepTrack->next != Head) 
-		keepTrack = keepTrack->next;
-	keepTrack->next = n;
-}
-
-int CircularLinkedList::CountingNodes(void) {
-	int k(0);
-	auto* keepTrack = Head->next;
-	while (keepTrack != Head) {
-		keepTrack = keepTrack->next;
-		k++;
-	}
-	return k;
-}
-
-void CircularLinkedList::Delete(int pos) {
-	if (pos < 0 || Head == Head->next || this->CountingNodes() - 1 < pos)
-		return;
-	if (pos == 0) {
-		if ((Head->next)->next == Head)
-			Head->next == Head;
-		else
-			Head->next = (Head->next)->next;
-	}
-	int count(0);
-	Node* pre = nullptr;
-	auto* curr = Head->next;
-	for (count; count < pos + 1 && curr != Head; count++) {
-		pre = curr;
-		curr = curr->next;
-	}
-	pre->next = curr->next;
-}
-
-void CircularLinkedList::DeleteHead() {
-	Head = Head->next;
-}
-
-
 //todo -> linkedLists :
 
 void linkedList::create(std::vector<int> vex) {
 	Head->Data = vex[0];
 	for (int i = 1; i < vex.size(); i++)
-		insertByTail(vex[i]);
+		appendByTail(vex[i]);
 }
 
 void linkedList::append(int x) {
@@ -84,7 +19,23 @@ void linkedList::append(int x) {
 	q->next = p;
 }
 
-void linkedList::insertByTail(int x) {
+void linkedList::insert(int index, int value) {
+	int k = 1;
+	Node* n = new Node;
+	n->Data = value;
+	n->next = nullptr;
+	Node* p = Head;
+	Node* q = Head->next;
+	while (k != index) {
+		p = p->next;
+		q = q->next;
+		k++;
+	}
+	p->next = n;
+	n->next = q;
+}
+
+void linkedList::appendByTail(int x) {
 	Node* n = new Node;
 	n->Data = x;
 	n->next = nullptr;
@@ -92,7 +43,7 @@ void linkedList::insertByTail(int x) {
 	Tail = n;
 }
 
-void linkedList::insertIfSorted(int x) {
+void linkedList::appendifSorted(int x) {
 	Node* n = new Node;
 	Node* keep = Head;
 	Node* keep_pre = nullptr;
@@ -107,7 +58,7 @@ void linkedList::insertIfSorted(int x) {
 		Head = n;
 	}
 	else if (keep == Tail)
-		this->insertByTail(x);
+		this->appendByTail(x);
 	else {
 		keep_pre->next = n;
 		n->next = keep;
@@ -327,3 +278,141 @@ void linkedList::reverse_two(void) {
 }
 
 void linkedList::merge(linkedList* list) { Tail->next = list->Head; }
+
+// todo->circularLinkedLists :
+
+CircularLinkedList::CircularLinkedList(void) {
+	Head->next = Head;
+	Head->Data = 0;
+}
+
+void CircularLinkedList::create(std::vector<int> vex) {
+	for (int i = 0; i < vex.size(); i++)
+		append(vex[i]);
+}
+
+void CircularLinkedList::Display(void) {
+	auto* keepTrack = Head->next;
+	while (keepTrack != Head) {
+		std::cout << keepTrack->Data << std::endl;
+		keepTrack = keepTrack->next;
+	}
+}
+
+void CircularLinkedList::append(int value) {
+	auto* n = new Node;
+	auto* keepTrack = Head;
+	n->Data = value;
+	n->next = Head;
+	while (keepTrack->next != Head)
+		keepTrack = keepTrack->next;
+	keepTrack->next = n;
+}
+
+int CircularLinkedList::CountingNodes(void) {
+	int k(0);
+	auto* keepTrack = Head->next;
+	while (keepTrack != Head) {
+		keepTrack = keepTrack->next;
+		k++;
+	}
+	return k;
+}
+
+void CircularLinkedList::Delete(int pos) {
+	if (pos < 0 || Head == Head->next || this->CountingNodes() - 1 < pos)
+		return;
+	if (pos == 0) {
+		if ((Head->next)->next == Head)
+			Head->next == Head;
+		else
+			Head->next = (Head->next)->next;
+	}
+	int count(0);
+	Node* pre = nullptr;
+	auto* curr = Head->next;
+	for (count; count < pos && curr != Head; count++) {
+		pre = curr;
+		curr = curr->next;
+	}
+	pre->next = curr->next;
+}
+
+void CircularLinkedList::DeleteHead() {
+	Head = Head->next;
+}
+
+//todo : DoubleLinkedLists
+
+void DoubleLinkedList::create(std::vector<int> vex) {
+	for (int i = 0; i < vex.size(); i++)
+		this->append(vex[i]);
+}
+
+void DoubleLinkedList::Display(void) {
+	auto* p = Head;
+	while (p)
+	{
+		std::cout << p->Data << ' ';
+		p = p->next;
+	}
+}
+
+void DoubleLinkedList::revDisplay(void) {
+	DoubleNode* keepTrack = Tail;
+	do {
+		std::cout << keepTrack->Data << " ";
+		keepTrack = keepTrack->prev;
+	} while (keepTrack);
+}
+
+void DoubleLinkedList::insert(int index, int value) {
+	DoubleNode* n = new DoubleNode;
+	n->Data = value;
+	n->next = nullptr;
+	n->prev = nullptr;
+	DoubleNode* p = nullptr;
+	DoubleNode* q = Head;
+	int k(-1);
+	while (k != index) {
+		p = q;
+		q = q->next;
+		k++;
+	}
+	if (!p && q) {
+		n->next = Head;
+		Head = n;
+	}
+	else {
+		p->next = n;
+		n->prev = p;
+		n->next = q;
+	}
+}
+
+void DoubleLinkedList::append(int x) {
+	auto* n = new DoubleNode;
+	n->next = nullptr;
+	n->prev = nullptr;
+	n->Data = x;
+	auto* p = Head;
+	while (p->next)
+		p = p->next;
+	p->next = n;
+	n->prev = p;
+	Tail = n;
+}
+
+void DoubleLinkedList::Delete(int position) {
+	int k(0);
+	DoubleNode* p = Head;
+	DoubleNode* q = Head->next;
+	while (k != position) {
+		p = q;
+		q = q->next;
+		k++;
+	}
+	p->next = q->next;
+}
+
+void DoubleLinkedList::DeleteHead(void) { Head = Head->next; }
