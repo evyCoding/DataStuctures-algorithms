d8337ec62c75ae745f06a52ae6ff2ee774eef252
yahya chami <123872563+evyCoding@users.noreply.github.com>
2023-08-27 01:09:16 +0100

    Create main.cpp

---
+++ stack/main.cpp
@@ -0,0 +1,12 @@
+#include "STACK.h"
+
+int main() {
+	auto* st = new St;
+	for (int i = 10; i < 16; i++)
+		st->push(i);
+	//st->Display();
+	std::cout << st->per(3);
+	//std::cout << st->countingNodes(); // sup == 6
+}
+
+
