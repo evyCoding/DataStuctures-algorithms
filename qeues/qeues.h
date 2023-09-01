fa9f316e1c36b602e6d6cacfaf4e1ec3ef752176
yahya chami <123872563+evyCoding@users.noreply.github.com>
2023-09-01 16:51:32 +0100

    Update qeues.h

--- qeues/qeues.h
+++ qeues/qeues.h
@@ -1,14 +1,25 @@
+#include <iostream>
+
 #pragma once
 
-class deque
+class qeues_with_Array
 {
 private:
-	int* Arr {};
-	size_t SIZE {};
+	int* Arr{};
+	size_t SIZE{};
 	int front = 0;
-	int rear = SIZE - 1;
+	int rear = 0;
 public:
-	deque();
-	
+	// constructor && destructor
+	qeues_with_Array();
+	~qeues_with_Array();
+	// function :
+	void pop(void);
+	void push(int x);
+	void log();
+	int Front(void);
+	int Back(void);
+	int size(void);
+	bool Empty(void);
 };
 
