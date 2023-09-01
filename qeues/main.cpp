d0024b87496ceb14c1cffe992a6250af27e67c0b
yahya chami <123872563+evyCoding@users.noreply.github.com>
2023-09-01 16:50:48 +0100

    Update main.cpp

--- qeues/main.cpp
+++ qeues/main.cpp
@@ -1,3 +1,14 @@
 #include "qeues.h"
 #include <vector>
 #include <iostream>
+
+
+
+int main() {
+	auto* myQeue = new qeues_with_Array;
+	myQeue->push(1);
+	myQeue->push(2);
+	myQeue->push(3);
+	myQeue->push(4);
+	myQeue->log();
+}
